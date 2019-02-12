2659cd51a0ba802a3470136b8cdad660f1d65f30
1103409364 <1103409364@qq.com>
2019-02-12 16:42:30 +0800

    first commit

---
+++ Balloon-Hit/js/HtmlUtil.js
@@ -0,0 +1,83 @@
+var HtmlUtil = {
+    log: console.log.bind(console),
+    /*1.用浏览器内部转换器实现html转码*/
+    htmlEncode: function (html) {
+        //1.首先动态创建一个容器标签元素，如DIV
+        var temp = document.createElement("div");
+        //2.然后将要转换的字符串设置为这个元素的innerText(ie支持)或者textContent(火狐，google支持)
+        (temp.textContent != undefined) ? (temp.textContent = html) : (temp.innerText = html);
+        //3.最后返回这个元素的innerHTML，即得到经过HTML编码转换的字符串了
+        var output = temp.innerHTML;
+        temp = null;
+        return output;
+    },
+    /*2.用浏览器内部转换器实现html解码*/
+    htmlDecode: function (text) {
+        //1.首先动态创建一个容器标签元素，如DIV
+        var temp = document.createElement("div");
+        //2.然后将要转换的字符串设置为这个元素的innerHTML(ie，火狐，google都支持)
+        temp.innerHTML = text;
+        //3.最后返回这个元素的innerText(ie支持)或者textContent(火狐，google支持)，即得到经过HTML解码的字符串了。
+        var output = temp.innerText || temp.textContent;
+        temp = null;
+        return output;
+    },
+    /*3.用正则表达式实现html转码*/
+    htmlEncodeByRegExp: function (str) {
+        var s = "";
+        if (str.length == 0) return "";
+        s = str.replace(/&/g, "&amp;");
+        s = s.replace(/</g, "&lt;");
+        s = s.replace(/>/g, "&gt;");
+        s = s.replace(/ /g, "&nbsp;");
+        s = s.replace(/\'/g, "&#39;");
+        s = s.replace(/\"/g, "&quot;");
+        return s;
+    },
+    /*4.用正则表达式实现html解码*/
+    htmlDecodeByRegExp: function (str) {
+        var s = "";
+        if (str.length == 0) return "";
+        s = str.replace(/&amp;/g, "&");
+        s = s.replace(/&lt;/g, "<");
+        s = s.replace(/&gt;/g, ">");
+        s = s.replace(/&nbsp;/g, " ");
+        s = s.replace(/&#39;/g, "\'");
+        s = s.replace(/&quot;/g, "\"");
+        return s;
+    },
+    // 获得数组最大值
+    maxValue: function (arr) {
+        let max = arr[0];
+        for (let i = 0; i < arr.length; i++) {
+            max = Number(arr[i]) <= max ? max : Number(arr[i]);
+        }
+        return max;
+    },
+    // 判断是不是数字
+    isNumber: function (n) {
+        return !isNaN(Number(n));
+    },
+    //class开关
+    toggle: function (element, cls) {
+        if (element.classList.contains(cls)) {
+            element.classList.remove(cls)
+        } else {
+            element.classList.add(cls)
+        }
+    },
+    // 获取min到max之间的随机数
+    getRandom: function (min, max) {
+        var a = Math.floor(Math.random() * (max - min + 1)) + min;
+        return a;
+    },
+    //获取计算后的样式elem dom元素  prop 元素属性
+    getStyle: function (elem, prop) {
+        if (window.getComputedStyle) {
+            return window.getComputedStyle(elem, null)[prop];//null可以替换成伪类元素，可以得到伪类元素的属性值
+        } else {
+            return elem.currentStyle[prop];
+        }
+    }
+
+};
