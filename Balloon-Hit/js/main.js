8077adafb335273e5af67ae6b0f57d708c0211db
1103409364 <1103409364@qq.com>
2019-02-15 23:09:14 +0800

    增加页面加载前等待

--- Balloon-Hit/js/main.js
+++ Balloon-Hit/js/main.js
@@ -140,3 +140,22 @@ var __main = function () {
 }
 
 __main();
+
+//在页面未加载完毕之前显示的loading Html自定义内容
+
+var _LoadingHtml = '页面加载中，请等待...';
+
+//呈现loading效果
+
+document.write(_LoadingHtml);
+
+//监听加载状态改变
+document.onreadystatechange = completeLoading;
+
+//加载状态为complete时移除loading效果
+function completeLoading() {
+    if (document.readyState == "complete") {
+        var loadingMask = document.getElementById('loadingDiv');
+        loadingMask.parentNode.removeChild(loadingMask);
+    }
+}
