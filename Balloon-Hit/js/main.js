b7df17aeab0f1f78858cd4159ef83ac2ae87a2f1
1103409364 <1103409364@qq.com>
2019-03-17 23:25:33 +0800

    重构代码,使用精灵图减少请求数

--- Balloon-Hit/js/main.js
+++ Balloon-Hit/js/main.js
@@ -1,154 +1,124 @@
 var log = console.log.bind(console);
 
+var balloonContainer = document.getElementById("balloonContainer"),
+    balloonContainerWidth = balloonContainer.clientWidth,
+    timeWrap = document.getElementById("time"),
+    boomAudio = document.getElementById("boom"),
+    scoreWrap = document.getElementById("score"),
+    frameNumberWrap = document.getElementById("fnum"),
+    speedWrap = document.getElementById("speed");
+const MAXTIME = 50;
+// 总分和时间
+var score = 0,
+    time = MAXTIME,
+    restart = false;
+
 // 气球类
 function Balloon() {
-    this.balloonContainer = document.getElementById("balloonContainer");
-    this.balloonContainerWidth = parseInt(HtmlUtil.getStyle(this.balloonContainer, "width"));
-    this.left = HtmlUtil.getRandom(0, this.balloonContainerWidth - 63);
+    // 初始位置
+    this.left = HtmlUtil.getRandom(0, balloonContainerWidth - 63);
     this.bottom = -200;
-    this.speed = 3;
-    this.fps = document.getElementById("fps").textContent;
-    this.color = HtmlUtil.getRandom(1, 3);
-    this.score = document.getElementById("score").textContent;
-    // 分数值
+
+
+    // 分值随机
     this.value = HtmlUtil.getRandom(1, 10);
-    // 气球div
+    // 分值越高的速度越快
+    if (this.value <= 3) {
+        this.speed = 3;
+    } else {
+        this.speed = this.value;
+    }
+    // speedWrap.innerHTML = this.speed;
+
+    this.initinal();
+    this.fly();
+}
+//添加一个初始化方法
+Balloon.prototype.initinal = function () {
+    //创建气球div容器
     this.dom = document.createElement("div");
-    // 气球的有效点击区域
+    //创建气球可点击区域
     this.targetDom = document.createElement("div");
-
-    this.dom.style =
-        "background: url(img/" + this.color + ".png) 0 0 no-repeat;" +
-        "background-size: 62px 234.5px;" +
-        "width: 63px; height: 234.5px;" +
-        "text-align:center;" +
-        "font-size: 30px;" +
-        "color: #fff;" +
-        "position: absolute;" +
-        "left: " + this.left + "px;" + "bottom: " + this.bottom + "px;";
-
-    this.targetDom.style =
-        "width: 63px;" +
-        "height: 60px;" +
-        "padding-top: 10px;" +
-        "position: absolute;" +
-        "left: 0" +
-        "top: 0";
-
-    this.targetDom.textContent = this.value;
+    // 显示分数
+    this.targetDom.innerHTML = this.value;
     this.dom.appendChild(this.targetDom);
+    // 添加类名
+    this.targetDom.className = "target";
+    this.dom.className = "ballon";
+    this.dom.style.left = this.left + "px";
+    this.dom.style.bottom = this.bottom + "px";
+    // 添加气球到容器中
+    balloonContainer.appendChild(this.dom);
+    // 设置背景定位,从精灵图上随机取一个气球作为背景
+    var x = -(HtmlUtil.getRandom(1, 3) - 1) * 63,
+        y = 0;
+    this.dom.style.backgroundPosition = x + "px " + y;
 
-    this.balloonContainer.appendChild(this.dom);
-    this.targetDom.addEventListener("click", function () {
-        var boom = document.getElementById("boom");
-        var scoreElement = document.getElementById("score");
-        var score = parseInt(scoreElement.textContent);
-        scoreElement.textContent = parseInt(this.textContent) + score;
-        // 没有load会导致短时间play两次，第二次无法播放的问题
-        boom.load();
-        boom.play();
-        this.parentNode.style = "display: none";
-    });
-
-    // 备份this
     var _this = this;
+    // 绑定监听事件
+    this.targetDom.onclick = function () {
+        _this.boom();
+        // 累加分数
+        score += _this.value;
+        scoreWrap.innerHTML = score;
+        // 没有load会导致短时间play两次，第二次无法播放的问题
+        boomAudio.load();
+        boomAudio.play();
 
-    var id = setInterval(function () {
+    }
+}
+// 气球起飞方法
+Balloon.prototype.fly = function () {
+    var _this = this;
+    this.timer = setInterval(function () {
         if (_this.bottom < 1000) {
-            // 根据分数来调节速度
-            if (_this.score > 50) {
-                _this.speed = 5;
-            }
-            if (_this.score > 100) {
-                _this.speed = 8;
-            }
-            if (_this.score > 200) {
-                _this.speed = 11;
-            }
-            if (_this.score > 300) {
-                _this.speed = 13;
-            }
-
+            // 更改bottom让气球动起来
             _this.bottom += _this.speed;
-        } else {
-            clearInterval(id);
+            _this.dom.style.bottom = _this.bottom + "px";
         }
-
-        _this.dom.style.bottom = _this.bottom + "px";
-    }, 1000 / _this.fps);
+        if (time == 0 || _this.bottom >= 1000 || restart == true) {
+            _this.boom();
+        }
+    }, 20);
 }
 
+// 爆炸方法
+Balloon.prototype.boom = function () {
+    clearInterval(this.timer);
+    balloonContainer.removeChild(this.dom);
+}
+
+// 游戏开始
 var gameStart = function () {
-    var balloonContainer = document.getElementById("balloonContainer");
-
-    var d = new Date();
-    var timeEle = document.getElementById("time");
-    // 游戏时间限制
-    var maxTime = timeEle.textContent;
-    var id = setInterval(function () {
-        var time = timeEle.textContent;
-
-        if (time > 0) {
-            new Balloon();
-            var newDate = new Date();
-            var second = Math.floor((newDate - d) / 1000);
-
-            timeEle.textContent = maxTime - second;
-        } else {
-            // 时间到游戏结束
-            clearInterval(id);
+    // 重置总分和时间
+    score = 0;
+    time = MAXTIME;
+    // bgm.load();
+    bgm.play();
+    // 帧编号
+    var frameNumber = 0;
+    // 500ms出来一个气球
+    var timer = setInterval(function () {
+        frameNumber++;
+        frameNumberWrap.innerHTML = frameNumber;
+        if (frameNumber % 2 == 0) {
+            // 倒计时
+            time--;
+            timeWrap.innerHTML = time
+        }
+        new Balloon();
+        
+        restart = false;
+        if (time == 0) {
+            clearInterval(timer);
             var bgm = document.getElementById("bgm");
             bgm.pause();
-            balloonContainer.innerHTML = "";
-            alert("GAME OVER!!");
+            scoreWrap.innerHTML += " GAME OVER";
+            // alert("GAME OVER 你的分数是" + score);
         }
     }, 500);
-
-    return id;
+    return timer;
 }
-
-var addEvent = function (id) {
-    var btn = document.getElementById("btn");
-    btn.addEventListener("click", function () {
-        // location.reload();
-        // 重新开始游戏，清除上一次的定时器
-        clearInterval(id);
-        initinal();
-        var newid = gameStart();
-        addEvent(newid);
-    });
-}
-
-var initinal = function () {
-    // 游戏时间限制
-    var time = 50;
-    var fps = 50;
-    var ini = `
-        <audio id="bgm" src="audio/bgm.mp3" autoplay loop></audio>
-        <audio id="boom" src="audio/boom.mp3"></audio>
-        <div id="status">
-            <button id="btn">restart</button>
-            fps:
-            <span id="fps">${fps}</span>
-            time:
-            <span id="time">${time}</span>
-            score:
-            <span id="score">0</span>
-        </div>
-
-        <div id="balloonContainer"></div>
-    `;
-
-    var container = document.getElementById("container");
-    container.innerHTML = ini;
-}
-
-var __main = function () {
-    initinal();
-    var id = gameStart();
-    addEvent(id);
-}
-
 
 //在页面未加载完毕之前显示的loading Html自定义内容
 var _LoadingHtml = "one thousand years later...";
@@ -161,6 +131,13 @@ loadingMask.innerHTML = _LoadingHtml;
 window.onload = function () {
     // 移除loading效果
     loadingMask.parentNode.removeChild(loadingMask);
-    __main();
-}
+    var timer = gameStart();
 
+    var btn = document.getElementById("btn");
+    // 重新开始按钮绑定事件
+    btn.onclick = function () {
+        restart = true;
+        clearInterval(timer);
+        timer = gameStart();
+    };
+}
